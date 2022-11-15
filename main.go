ae54d0a2d3838ba5a2096c82bf9260d4e30eda5b
TJD1604 <118253067+TJD1604@users.noreply.github.com>
2022-11-15 12:38:27 +0100

    Update main.go

--- main.go
+++ main.go
@@ -19,30 +19,12 @@ func main() {
 	}
 
 	//println(file)
-
-	start, end := time.Now(), time.Now().Add(12*30*24*time.Hour)
+	houresLeft := 24 - time.Now().Hour()
+	start, end := time.Now(), time.Now().Add(time.Hour*time.Duration(houresLeft))
 
 	c := gocal.NewParser(strings.NewReader(file))
 	c.Start, c.End = &start, &end
 	c.Parse()
 
-	type lesson struct {
-		name    string
-		room    string
-		teacher string
-		start   *time.Time
-		end     *time.Time
-	}
-
-	//array := make([]lesson, len(c.Events))
-	for _, e := range c.Events {
-		fmt.Println("%v", strings.SplitN(e.Summary, ";", 4))
-		/*
-			array[key].name = strings.SplitN(e.Summary, ";", 4)[1]
-			array[key].teacher = strings.SplitN(e.Summary, ";", 4)[3]
-			array[key].start = e.Start
-			array[key].end = e.End
-		*/
-
-	}
+	fmt.Print(helper.ParseIcsFile(c))
 }
