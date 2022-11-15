f89c7a5996c4747a0d0c436050a209e51224c560
TJD1604 <118253067+TJD1604@users.noreply.github.com>
2022-11-15 12:59:19 +0100

    Update main.go

--- main.go
+++ main.go
@@ -14,20 +14,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-<<<<<<< HEAD
 	fmt.Print(helper.ParseIcsFile(file))
-=======
-
-	//println(file)
-	houresLeft := 24 - time.Now().Hour()
-	start, end := time.Now(), time.Now().Add(time.Hour*time.Duration(houresLeft))
-
-	c := gocal.NewParser(strings.NewReader(file))
-	c.Start, c.End = &start, &end
-	c.Parse()
-
-	fmt.Print(helper.ParseIcsFile(c))
-
-	fmt.Scanln()
->>>>>>> 98e87a366f8ae1eced6ed0f47a0647946f1f7d3c
 }
