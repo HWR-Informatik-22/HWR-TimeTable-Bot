daff7511ab9b2bdbd3190f3b7454468ecfdd6b9a
Lucas <Lucas@Heinschke.com>
2022-11-15 15:17:14 +0100

    fixed stupid shit

--- helper/embed.go
+++ helper/embed.go
@@ -19,7 +19,7 @@ func createEmbed(lessons []Lesson, course string) {
 func getEmbedFields(lessons []Lesson, course string) []*discordgo.MessageEmbedField {
 	fields := make([]*discordgo.MessageEmbedField, len(lessons))
 	for i, e := range lessons {
-		fields[i] = *discordgo.MessageEmbedField{
+		fields[i] = &discordgo.MessageEmbedField{
 			Name:   fmt.Sprintf("%s (%s)", e.name, e.teacher),
 			Value:  fmt.Sprintf("%s\n%s\n%s", e.start, e.end, e.room),
 			Inline: false,
