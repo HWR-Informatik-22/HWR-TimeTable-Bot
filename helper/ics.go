35d269e492ea32a38da2cc83b3e46b274c01fa95
TJD1604 <118253067+TJD1604@users.noreply.github.com>
2022-11-15 12:38:31 +0100

    Update ics.go

--- helper/ics.go
+++ helper/ics.go
@@ -5,7 +5,19 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/apognu/gocal"
 )
+
+type lesson struct {
+	name    string
+	room    string
+	teacher string
+	start   *time.Time
+	end     *time.Time
+}
 
 func GetIcsFile(course string) (string, error) {
 	url := fmt.Sprintf("%s%s", os.Getenv("BASE_ICS_URL"), course)
@@ -24,4 +36,19 @@ func GetIcsFile(course string) (string, error) {
 	}
 
 	return string(b), nil
+}
+
+func ParseIcsFile(ics *gocal.Gocal) []lesson {
+	array := make([]lesson, len(ics.Events))
+	for key, e := range ics.Events {
+		split := strings.Split(e.Summary, ";")
+		array[key].name = split[1]
+		array[key].teacher = split[2]
+		if len(split) == 4 {
+			array[key].room = split[3]
+		}
+		array[key].start = e.Start
+		array[key].end = e.End
+	}
+	return array
 }
