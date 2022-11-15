98feb6c81d5561a371bb186dfcff744d1b9e86cf
TJD1604 <118253067+TJD1604@users.noreply.github.com>
2022-11-15 12:55:51 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"fmt"
 	"hwrbot/helper"
-	"strings"
-	"time"
 
-	"github.com/apognu/gocal"
 	"github.com/joho/godotenv"
 )
 
@@ -17,14 +14,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	//println(file)
-	houresLeft := 24 - time.Now().Hour()
-	start, end := time.Now(), time.Now().Add(time.Hour*time.Duration(houresLeft))
-
-	c := gocal.NewParser(strings.NewReader(file))
-	c.Start, c.End = &start, &end
-	c.Parse()
-
-	fmt.Print(helper.ParseIcsFile(c))
+	fmt.Print(helper.ParseIcsFile(file))
 }
