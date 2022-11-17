e61386a080d275707a4dc7cfb75542ba85c220f8
Lucas <Lucas@Heinschke.com>
2022-11-17 15:10:44 +0100

    added scheduled sending

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"hwrbot/helper"
 	"os"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/go-co-op/gocron"
 	"github.com/joho/godotenv"
 )
 
@@ -13,12 +14,20 @@ const course = "kursa"
 
 func main() {
 	godotenv.Load(".env")
+
 	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_BOT_TOKEN"))
 	if err != nil {
 		panic(err)
 	}
 	discord.Open()
 
+	s := gocron.NewScheduler(time.UTC)
+	s.Cron(os.Getenv("EXECUTE_INTERVAL")).Do(sendTimeTable, discord, "kursa")
+	s.Cron(os.Getenv("EXECUTE_INTERVAL")).Do(sendTimeTable, discord, "kursb")
+	s.StartBlocking()
+}
+
+func sendTimeTable(discord *discordgo.Session, course string) {
 	content, err := helper.GetIcsContent(course)
 	if err != nil {
 		panic(err)
@@ -26,8 +35,7 @@ func main() {
 
 	lessons, err := helper.ParseIcsContent(content)
 	if err != nil {
-		os.Exit(2)
-		return
+		panic(err)
 	}
 
 	embed := helper.CreateEmbed(lessons, course)
@@ -35,6 +43,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Scanln()
 }
